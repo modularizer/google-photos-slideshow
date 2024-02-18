2a9777deeb22297a64dd44393173f90e723cefed
modularizer <modularizer@gmail.com>
2024-02-17 23:40:00 -0800

    make pip installable

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Google Photos Slideshow
+# google-photos-slideshow
+
+NOTE: this project is in no way affiliated with or sponsored by Google Photos.
+
+## Quickstart
+### Install
+```commandline
+pip install google-photos-slideshow
+```
+
+### Run
+```commandline
+google-photos-slideshow
+```
+* The first time you run this, it will prompt you for the public url to your google photos album.
+* Future runs will use the same album unless you specify a different one using `--url`.
+
+### View
+Open a web browser and navigate to `http://localhost` to view the slideshow.
+
+### Options
+```commandline
+google-photos-slideshow --help
+```
--- google_photos_slideshow.py
+++ google_photos_slideshow.py
@@ -2,9 +2,11 @@ import asyncio
 import random
 import re
 import json
+import signal
 import time
 import logging
 from pathlib import Path
+import argparse
 
 import aiohttp
 from aiohttp import web
@@ -23,10 +25,10 @@ class Slideshow:
                  regex=r'https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9_\-\/]+',
                  host='localhost',
                  websocket_port=6789,
-                 http_port=80):
+                 port=80):
         self.host = host
         self.websocket_port = websocket_port
-        self.http_port = http_port
+        self.port = port
         self.url = url
         self.refresh_interval = refresh_interval
         self.regex = regex
@@ -79,7 +81,7 @@ class Slideshow:
         if self.urls and not self.paused:
             current_url = await self._next_url()
             await self._send_to_all(json.dumps({'url': current_url}))
-            logger.info(f"sleeping for {self.image_speed} seconds")
+            logger.debug(f"sleeping for {self.image_speed} seconds")
             await asyncio.sleep(self.image_speed)
 
     async def _register(self, websocket):
@@ -113,7 +115,7 @@ class Slideshow:
     async def run(self):
         await self._fetch_urls()
         while True:
-            logger.info(f"updating clients: {self.current_index}/{len(self.urls)}")
+            logger.debug(f"updating clients: {self.current_index}/{len(self.urls)}")
             await self._update_clients()
             # sleep
             await asyncio.sleep(0.1)
@@ -134,11 +136,16 @@ class Slideshow:
         self.setup_routes(app)
         runner = web.AppRunner(app)
         await runner.setup()
-        site = web.TCPSite(runner, self.host, self.http_port)
+        site = web.TCPSite(runner, self.host, self.port)
         await site.start()
-        logger.info(f"HTTP server started at {self.host}:{self.http_port}")
+        p = f":{self.port}" if self.port != 80 else ""
+        logger.info(f"Open your browser and go to http://{self.host}{p}")
 
     def serve(self):
+        # attach cleanup handler for graceful shutdown on Ctrl+C
+        signal.signal(signal.SIGINT, lambda s, f: self.cleanup())
+        signal.signal(signal.SIGTERM, lambda s, f: self.cleanup())
+
         logger.info(f"Starting websocket server at {self.host}:{self.websocket_port}")
         start_server = websockets.serve(self.websocket_handler, self.host, self.websocket_port)
         # start the http server using asyncio
@@ -148,19 +155,49 @@ class Slideshow:
             self.start_http_server(),
             self.run(),  # Assuming this is an async method related to your slideshow logic
         )
-        asyncio.get_event_loop().run_forever()
+        loop = asyncio.get_event_loop()
 
-        logger.info("Servers started.")
+        loop.run_forever()
+
+    def cleanup(self):
+        """Cleanup the servers"""
+        logger.info("Cleaning up servers")
+        asyncio.get_event_loop().stop()
 
 
 def main():
+    # use argparse to parse command line arguments
+    parser = argparse.ArgumentParser(description="Make a live slideshow from a publicly shared google photos album")
+    parser.add_argument("--port", type=int, default=80, help="The port for the http server")
+    parser.add_argument("--url", help="The google photos album link", type=str, default=None)
+    parser.add_argument("--image-speed", type=float, default=1, help="Time in seconds for each image")
+    parser.add_argument("--refresh-interval", type=int, default=5, help="Time in seconds to refresh the album link")
+    parser.add_argument("--regex", default=r'https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9_\-\/]+', help="The regex to match the image urls")
+    parser.add_argument("--host", default='localhost', help="The host to serve the slideshow")
+    parser.add_argument("--websocket-port", type=int, default=6789, help="The port for the websocket server")
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO)
-    # read url from file
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
     url_path = Path(__file__).parent / 'url.txt'
-    url = url_path.read_text().strip()
+    if args.url is not None:
+        url = args.url
+    elif not url_path.exists():
+        url = input("Enter the google photos album link: ")
+    else:
+        url = url_path.read_text().strip()
+    url_path.write_text(url)
 
     # start the slideshow
-    s = Slideshow(url)
+    s = Slideshow(url,
+                  image_speed=args.image_speed,
+                  refresh_interval=args.refresh_interval,
+                  regex=args.regex,
+                  host=args.host,
+                  websocket_port=args.websocket_port,
+                  port=args.port)
     s.serve()
 
 
